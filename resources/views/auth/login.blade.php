c5051776dd8877bcaf4b594c575607d296b55e19
jgcfmsilva <jgcfmsilva.jgs@gmail.com>
2025-04-29 16:34:03 +0100

    branch finished

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,8 +22,16 @@
             </div>
 
             <!-- Password -->
-            <div>
-                <label for="password" class="block text-sm font-medium text-gray-800">Password</label>
+            <div class="mt-3">
+                <div class="flex items-center justify-between">
+                    <label for="password" class="block text-sm font-medium text-gray-800">Password</label>
+                    <div class="cursor-pointer">
+                        <svg  id="togglePassword" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                    </div>
+                </div>
                 <input type="password" name="password" id="password" required
                     class="mt-1 block w-full rounded-md border-2 @error('password') border-red-500 @else border-gray-300 @enderror bg-white focus:ring-indigo-500 focus:border-indigo-500 text-gray-800 py-2 px-4" />
                 @error('password')
@@ -74,5 +82,5 @@
 @endsection
 
 @push('scripts')
-    @vite('resources/js/pages/auth/forgot-password.js')
+    @vite('resources/js/pages/auth/login.js')
 @endpush
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -3,51 +3,65 @@
 @section('title', 'Redefinir Senha')
 
 @section('content')
-    <div class="max-w-md mx-auto bg-white p-8 rounded-lg shadow-md">
-        <h1 class="text-2xl font-bold text-gray-800 mb-6">Set new password</h1>
 
-        @if (session('status'))
-            <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-6">
-                <div class="flex items-center">
-                    <svg class="h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                    </svg>
-                    <p>{{ session('status') }}</p>
-                </div>
-            </div>
-        @endif
 
-        <form method="POST" action="{{ route('password.update') }}" class="space-y-6">
-            @csrf
+    <form method="POST" action="{{ route('password.update') }}"
+        class="max-w-4xl mx-auto bg-white p-6 rounded-xl shadow-md space-y-6">
+        @csrf
+        <input type="hidden" name="token" value="{{ $token }}">
+        <div class="gap-6">
+            <!-- Email -->
+            <div>
+                <label for="email" class="block text-sm font-medium text-gray-800">Email</label>
+                <input type="email" name="email" id="email" value="{{ old('email', $email) }}" required
+                    class="mt-1 block w-full rounded-md border-2 @error('email') border-red-500 @else border-gray-300 @enderror bg-white focus:ring-indigo-500 focus:border-indigo-500 text-gray-800 py-2 px-4" />
+                @error('email')
                     <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
                 @enderror
-            <input type="hidden" name="token" value="{{ $token }}">
-            <div>
-                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                <input id="email" type="email" name="email" value="{{ old('email', $email) }}"
-                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 @error('email') border-red-500 @enderror py-2 px-3 border">
-                @error('email')
             </div>
 
-            <div>
-                <label for="password" class="block text-sm font-medium text-gray-700">New password</label>
-                <input id="password" type="password" name="password" required
-                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 @error('password') border-red-500 @enderror py-2 px-3 border">
+            <!-- Password -->
+            <div class="mt-3">
+                <div class="flex items-center justify-between">
+                    <label for="password" class="block text-sm font-medium text-gray-800">Password</label>
+                </div>
+                <input type="password" name="password" id="password" required
+                    class="mt-1 block w-full rounded-md border-2 @error('password') border-red-500 @else border-gray-300 @enderror bg-white focus:ring-indigo-500 focus:border-indigo-500 text-gray-800 py-2 px-4" />
                 @error('password')
                     <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
                 @enderror
+
             </div>
 
-                <label for="password-confirm" class="block text-sm font-medium text-gray-700">Confirm password</label>
+            <!-- Confirm Password -->
+            <div class="mt-3">
+                <div class="flex items-center justify-between">
+                    <label for="password-confirm" class="block text-sm font-medium text-gray-700">Confirm password</label>
+
+                    <div class="cursor-pointer">
+                        <svg id="togglePassword" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500"
+                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                    </div>
+                </div>
+
                 <input id="password-confirm" type="password" name="password_confirmation" required
-                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 py-2 px-3 border">
+                class="mt-1 block w-full rounded-md border-2 @error('password') border-red-500 @else border-gray-300 @enderror bg-white focus:ring-indigo-500 focus:border-indigo-500 text-gray-800 py-2 px-4" />
             </div>
 
-            <div>
-            <div>
+            <!-- submit -->
+            <div class="mt-3">
                 <button type="submit" class="w-full rounded btn btn-primary">Set new password</button>
-
             </div>
-        </form>
-    </div>
+
+        </div>
+    </form>
 @endsection
+
+@push('scripts')
+    @vite('resources/js/pages/auth/reset-password.js')
+@endpush
