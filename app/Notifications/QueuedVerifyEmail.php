197c5d432b36973952d6d2ff63a2a740e9340722
jgcfmsilva <jgcfmsilva.jgs@gmail.com>
2025-06-26 22:40:42 +0100

    queue server email fixes

--- app/Notifications/QueuedVerifyEmail.php
+++ app/Notifications/QueuedVerifyEmail.php
@@ -8,4 +8,18 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class QueuedVerifyEmail extends VerifyEmail implements ShouldQueue
 {
     public $queue = 'emails';
+
+    /**
+     * The queue connection that should be used for the notification.
+     *
+     * @var string|null
+     */
+    public $connection = null; // or set to 'database', 'redis', etc.
+
+    /**
+     * The number of seconds to delay before the job should be processed.
+     *
+     * @var \DateTimeInterface|\DateInterval|int|null
+     */
+    public $delay = null;
 }
